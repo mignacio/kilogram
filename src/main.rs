44490e6179b9f9252516aa8fc77cea1b6aae51ee
Ignacio Moya <moyaignacioj@gmail.com>
2024-06-21 15:36:14 -0300

    Minimal version with functional ascii art graphics to represent the bar.

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,16 @@ use crate::plate::*;
 
 fn main() {
     let plate_stack: [Plate; 10] = [
-        Plate::new(25.0, PlateColor::Red,    0, true),
-        Plate::new(20.0, PlateColor::Blue,   0, true),
-        Plate::new(15.0, PlateColor::Yellow, 0, true),
-        Plate::new(10.0, PlateColor::Green,  0, true),
-        Plate::new(5.0, PlateColor::White,   0, true),
-        Plate::new(2.5, PlateColor::Red,     0, false),
-        Plate::new(2.0, PlateColor::Blue,    0, false),
-        Plate::new(1.5, PlateColor::Yellow,  0, false),
-        Plate::new(1.0, PlateColor::Green,   0, false),
-        Plate::new(0.5, PlateColor::White,   0, false)
+        Plate::new(25.0, PlateColor::Red,    0, AsciiPlate::Plate1),
+        Plate::new(20.0, PlateColor::Blue,   0, AsciiPlate::Plate2),
+        Plate::new(15.0, PlateColor::Yellow, 0, AsciiPlate::Plate3),
+        Plate::new(10.0, PlateColor::Green,  0, AsciiPlate::Plate3),
+        Plate::new(5.0, PlateColor::White,   0, AsciiPlate::Plate4),
+        Plate::new(2.5, PlateColor::Red,     0, AsciiPlate::Plate4),
+        Plate::new(2.0, PlateColor::Blue,    0, AsciiPlate::Plate5),
+        Plate::new(1.5, PlateColor::Yellow,  0, AsciiPlate::Plate5),
+        Plate::new(1.0, PlateColor::Green,   0, AsciiPlate::Plate6),
+        Plate::new(0.5, PlateColor::White,   0, AsciiPlate::Plate6)
     ];
     let bar_weight: f32 = 20.0;
 
@@ -38,8 +38,8 @@ fn main() {
         },
     };
     
-    if total_weight < bar_weight {
-        println!("Weight must be greater than the weight of the bar ({}kg).", bar_weight);
+    if !(total_weight >= (bar_weight + 5.0)) {
+        println!("Weight must be greater or equal than the weight of the bar with collars ({})kg).", bar_weight + 6.0);
         return
     }
 
@@ -50,15 +50,38 @@ fn main() {
         total_weight -= 2.5;
     }
 
-    println!("     |     ");
-    println!("     =     ");
+    //Vector that represents how the bar is loaded with plates.
+    //The first element is the plate closest to the bar.
+    let mut ascii_plates: Vec<Plate> = Vec::new();
+
     for mut plate in plate_stack {
         plate.quantity = (total_weight / plate.get_weight()).floor() as i32;
         total_weight -= plate.quantity as f32 * plate.get_weight();
-        //println!("{}", plate.to_string());
         for _n in 0..plate.quantity{
-            println!("{}", plate.to_ascii_graphics()); 
+            ascii_plates.push(plate);
         }
     }
-    println!("     |     ");
-}
+
+    const BAR: &[&str; 7] = &[
+        "                  ",
+        "                  ",
+        "                  ",
+        "=————————————————=",
+        "                  ",
+        "                  ",
+        "                  ",
+    ];
+
+    for n in 0..7{
+        for ascii_plate in ascii_plates.iter().rev(){
+            print!("\x1b[9{}m{}\x1b[0m", ascii_plate.get_color().to_ansi_code(), ascii_plate.get_ascii_art().to_ascii_arr()[n]);
+        }
+
+        print!("{}", BAR[n]);
+
+        for ascii_plate in ascii_plates.iter(){
+            print!("\x1b[9{}m{}\x1b[0m", ascii_plate.get_color().to_ansi_code(), ascii_plate.get_ascii_art().to_ascii_arr()[n]);
+        }
+        println!();
+    }
+} 
--- src/plate.rs
+++ src/plate.rs
@@ -8,57 +8,44 @@ pub enum PlateColor{
 }
 
 impl PlateColor{
-    pub fn to_string(&self) -> String{
+    pub fn to_ansi_code(&self) -> &str{
         match self{
-            PlateColor::Red=>return "Red".to_string(),
-            PlateColor::Blue=>return "Blue".to_string(),
-            PlateColor::Yellow=>return "Yellow".to_string(),
-            PlateColor::Green=>return "Green".to_string(),
-            PlateColor::White=>return "White".to_string()
-        }
-    }
-
-    pub fn to_ansi_code(&self) -> String{
-        match self{
-            PlateColor::Red=>return "1".to_string(),
-            PlateColor::Blue=>return "4".to_string(),
-            PlateColor::Yellow=>return "3".to_string(),
-            PlateColor::Green=>return "2".to_string(),
-            PlateColor::White=>return "7".to_string()
+            PlateColor::Red=>return "1",
+            PlateColor::Blue=>return "4",
+            PlateColor::Yellow=>return "3",
+            PlateColor::Green=>return "2",
+            PlateColor::White=>return "7"
         }
     }
 }
 
+#[derive(Clone, Copy)]
 pub struct Plate{
     weight: f32,
     color: PlateColor,
     pub quantity: i32,
-    is_big: bool
+    ascii_art: AsciiPlate
 
 }
 
 impl Plate{
-    pub fn new(weight: f32, color: PlateColor, quantity: i32, is_big: bool) -> Self{
+    pub fn new(weight: f32, color: PlateColor, quantity: i32, ascii_art: AsciiPlate) -> Self{
         Self{
             weight,
             color,
             quantity,
-            is_big
+            ascii_art
         }
     }
 
-    pub fn to_ascii_graphics(&self) -> String{
-        if self.is_big {
-            return format!("  \x1b[10{}m\x1b[3{}m=======\x1b[0m  ", self.color.to_ansi_code(), self.color.to_ansi_code());
-        } else {
-            return format!("    \x1b[10{}m\x1b[3{}m---\x1b[0m    ", self.color.to_ansi_code(), self.color.to_ansi_code());
-        }
+    pub fn get_ascii_art(&self) -> AsciiPlate{
+        return self.ascii_art
     }
-
+/*
     pub fn to_string(&self) -> String{
         return format!("\x1b[10{}m\x1b[30m{} {}kg\t{}\x1b[0m", self.color.to_ansi_code(), self.quantity, self.weight, self.color.to_string());
     }
-
+*/
     pub fn get_weight(&self) -> f32{
         self.weight
     }
@@ -67,3 +54,91 @@ impl Plate{
         self.color
     }
 }
+
+#[derive(Clone, Copy)]
+pub enum AsciiPlate{
+    Plate1,
+    Plate2,
+    Plate3,
+    Plate4,
+    Plate5,
+    Plate6
+}
+
+impl AsciiPlate{
+    pub fn to_ascii_arr(&self) -> &[&str; 7] {
+        match self{
+            AsciiPlate::Plate1 => return ASCII_PLATE1,
+            AsciiPlate::Plate2 => return ASCII_PLATE2,
+            AsciiPlate::Plate3 => return ASCII_PLATE3,
+            AsciiPlate::Plate4 => return ASCII_PLATE4,
+            AsciiPlate::Plate5 => return ASCII_PLATE5,
+            AsciiPlate::Plate6 => return ASCII_PLATE6,
+        }
+    }
+}
+// Used for 25kg red plate.
+const ASCII_PLATE1: &[&str; 7] = &[
+    "|¦|",
+    "|¦|",
+    "|¦|",
+    "|¦|",
+    "|¦|",
+    "|¦|",
+    "|¦|",
+];
+
+//Used for 20 kg blue plate.
+const ASCII_PLATE2: &[&str; 7] = &[
+    "||",
+    "||",
+    "||",
+    "||",
+    "||",
+    "||",
+    "||",
+];
+
+//Used for 15 and 10kg yellow and green plates.
+const ASCII_PLATE3: &[&str; 7] = &[
+    "|",
+    "|",
+    "|",
+    "|",
+    "|",
+    "|",
+    "|",
+];
+
+//Used for 5 and 2.5kg white and red plates.
+const ASCII_PLATE4: &[&str; 7] = &[
+    " ",
+    "|",
+    "|",
+    "|",
+    "|",
+    "|",
+    " ",
+];
+
+//Used for 2 and 1.5kg blue and yellow plates.
+const ASCII_PLATE5: &[&str; 7] = &[
+    " ",
+    " ",
+    "|",
+    "|",
+    "|",
+    " ",
+    " ",
+];
+
+//Used for 1 and 0.5kg green and white plates.
+const ASCII_PLATE6: &[&str; 7] = &[
+    " ",
+    " ",
+    " ",
+    "|",
+    " ",
+    " ",
+    " ",
+];
